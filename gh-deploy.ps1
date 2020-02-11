b067ce879326cbc8a02dbf4e404cbed29ad9356e
Rohan Cragg <rohancragg@outlook.com>
2020-02-11 12:29:53 +0000

    deploying with --dirty to skip an error with using the git-revision-date plugin

--- gh-deploy.ps1
+++ gh-deploy.ps1
@@ -1,4 +1,6 @@
-﻿cd ..\rohancragg.github.io
-mkdocs gh-deploy --config-file ../blog-notes/mkdocs.yml --remote-branch master
+﻿mkdocs build --clean
+cd ..\rohancragg.github.io
+# deploying with --dirty to skip an error with using the git-revision-date plugin
+mkdocs gh-deploy --dirty --config-file ../blog-notes/mkdocs.yml --remote-branch master
 git reset --hard
 cd ..\blog-notes
